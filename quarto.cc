9045b1d3bcb16d0735e93e73f115f6058241c8f5
Maks Verver <maks@verver.ch>
2018-12-30 16:12:56 +0100

    Implement detection of Quarto (State::IsQuartoPossible()).

--- quarto.cc
+++ quarto.cc
@@ -2,6 +2,39 @@
 
 #include <iostream>
 
+namespace {
+
+// winning_lines[] encodes the horizontal, vertical and diagonal lines on the
+// board along which a Quarto can be formed.
+//
+// Each winning line consists of exactly four fields. Each field participates in
+// two or three winning lines, depending on whether the field lies on a diagonal
+// or not.
+//
+// The array below encodes the winning lines for each of 16 fields into a 64 bit
+// integer, where each group of 12 bits encodes the indices of three other
+// fields that are part of the same line (in addition to the field itself).
+//
+// For example, field 0 can form a winning line with fields 1, 2 and 3
+// (horizontally) or 4, 8, and 12 (vertically), or 5, 10, 15 (diagonally),
+// so the first element is 0xfa5c84321.
+static const unsigned long long winning_lines[16] = {
+    0xfa5c84321, 0x000d95320, 0x000ea6310, 0xc96fb7210,
+    0x000c80765, 0xfa0d91764, 0xc93ea2754, 0x000fb3654,
+    0x000c40ba9, 0xc63d51ba8, 0xf50e62b98, 0x000f73a98,
+    0x963840fed, 0x000951fec, 0x000a62fdc, 0xa50b73edc };
+
+// Pieces have four attributes (color, height, etc.) each of which has two
+// possible values (black/white, short/tall, etc.) This function takes a piece
+// number (a 4-bit integer) and returns an 8-bit bitmask with exactly 4 bits
+// set, corresponding with the attribute values for this piece.
+constexpr unsigned GetAttributeValues(int piece) {
+    assert(piece >= 0 && piece < 16);
+    return (piece << 4) | (piece ^ 0xf);
+}
+
+}  //namespace
+
 bool State::IsValid(Move move) const {
     switch (move.GetType()) {
     case Move::Type::SELECT:
@@ -26,10 +59,27 @@ bool State::IsValid(Move move) const {
 }
 
 bool State::IsQuartoPossible() const {
-    if (num_moves < 2 || Over()) return false;
-    CheckField(last_field);
-    // TODO: validate quarto involving `last_field`.
-    return true;
+    if (num_moves < 8 || Over()) return false;
+    assert(last_field >= 0);
+    int first_piece = fields[last_field];
+    assert(first_piece >= 0);
+    for (unsigned long long lines = winning_lines[last_field]; lines != 0;
+            lines >>= 12) {
+        unsigned line = lines & 0xfff;
+        unsigned common_attributes = GetAttributeValues(first_piece);
+        int n = 3;
+        for (; n > 0; --n) {
+            int next_piece = fields[line & 0xf];
+            if (next_piece < 0) break;  // next_field unoccupied
+            common_attributes &= GetAttributeValues(next_piece);
+            if (common_attributes == 0) break;  // no attribute in common
+            line >>= 4;
+        }
+        if (n == 0) {
+            return true;
+        }
+    }
+    return false;
 }
 
 std::vector<State::Move> State::ListValidMoves() const {
